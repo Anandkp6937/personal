def22bdebcff4016afa67b509432d2e0223981ff
anandkp6937 <anandkp6937@gmail.com>
2022-12-22 15:21:16 +0530

    added new html files

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ function autoclose(){
 
 menu.addEventListener("click",function(){
   navigationbar.style.display='flex';
+
   autoclose()
 })
 
@@ -20,38 +21,29 @@ xmark.addEventListener("click",function(){
   navigationbar.style.display='none';
 })
 
-let count=0;
-like.addEventListener("click",function(){
-if(count==0){
-  like.style.background=" #f10b6b";
-  console.log(count);
-count=1
-console.log(count);
-}
-else{
-like.style.background="var(--color)";
-like.style.transform="none";
-
-count=0;
-}
-})
+// let count=0;
+// like.addEventListener("click",function(){
+// if(count==0){
+//   like.style.background=" #f10b6b";
+//   console.log(count);
+// count=1
+// console.log(count);
+// }
+// else{
+// like.style.background="var(--color)";
+// like.style.transform="none";
+//
+// count=0;
+// }
+// })
 
 send.addEventListener("click",function(e){
   e.preventDefault();
-
-  console.log(inputname.value);
-  if(inputname.value=="" &&count==0){
-    alert("invalid operation");
+  if(inputname.value==""){
+    alert("invalid response found");
   }
   else{
-    if(count==1){
-      alert("thanku for your Like&response");
-      like.style.background="var(--color)";
-    }
-    if(count==0){
-      alert("thanku for your response");
-    }
+    alert("thanku for your response");
     inputname.value="";
   }
-
 })
